aeca926cd237c9458357887dfb8dbd44b64e3671
DanteXie <dantexie@outlook.com>
2017-06-24 19:03:41 -0400

    shooter button

--- shooter_button.js
+++ shooter_button.js
@@ -7,6 +7,7 @@ import {
   View,
   Text,
   TouchableOpacity,
+  TouchableWithoutFeedback,
   StyleSheet,
   Image,
   LayoutAnimation,
@@ -14,28 +15,130 @@ import {
 } from 'react-native';
 
 const window = Dimensions.get('window');
+var spinning = true;
+
+var spin;
 
 export default class ShooterButton extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
+      disabled: false,
       left: new Animated.Value(window.width/2-5),
       top: new Animated.Value(window.height/2-5),
+
+      arrow_left: new Animated.Value(40),
+      arrow_top: new Animated.Value(40),
+
+      opacity: new Animated.Value(0),
+      spinValue: new Animated.Value(0),
+    };
+  }
+
+  componentWillMount() {
+    this.state.spinValue.addListener(({value}) => this.spin_deg = value);
+  }
+
+  fadeIn() {
+    Animated.timing(
+      this.state.opacity,
+      {
+        toValue: 1,
+        duration: 100,
+        easing: Easing.linear
+      }
+    ).start(() => this.spin());
+  }
+
+  spin() {
+    if(spinning) {
+      let spinning_duration = Math.random() * (2000 - 700) + 700;
+      this.state.spinValue.setValue(0);
+      Animated.timing(
+        this.state.spinValue,
+        {
+          toValue: 1,
+          duration: spinning_duration,
+          easing: Easing.linear
+        }
+      ).start(() => this.spin());
     }
+  }
+
+  toRadians (angle) {
+    return angle * (Math.PI / 180);
+  }
+
+  arrowOnPress() {
+    spinning = false;
+    let spin_deg;
+
+    this.state.spinValue.stopAnimation((deg) => spin_deg = deg*360-45);
+
+    spin_deg = this.toRadians(spin_deg);
+    
+    y_offset = (window.height-45)*(Math.cos(spin_deg))+45;
+    x_offset = (window.width-45)*(Math.sin(spin_deg))+45;
+
+    this.fire(x_offset, -y_offset);
+  }
+
+  fire(x, y) {
+    this.state.spinValue.setValue(this.spin_deg);
+    Animated.timing(
+      this.state.arrow_left,
+      {
+        toValue: x,
+        duration: 700,
+        easing: Easing.quad.inOut
+      }
+    ).start();
+
+    Animated.timing(
+      this.state.arrow_top,
+      {
+        toValue: y,
+        duration: 700,
+        easing: Easing.quad.inOut
+      }
+    ).start();
   }
 
   render() {
     const _left = this.state.left;
-    let _top = this.state.top;
+    const _top = this.state.top;
+
+    const arrow_left = this.state.arrow_left;
+    const arrow_top = this.state.arrow_top;
+
+    const opacity = this.state.opacity;
+    spin = this.state.spinValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '360deg']
+    });
 
     return (
       <View style={[styles.container, this.props.container_style]}>
 
-        <Animated.View />
+        <Animated.View
+          style={[styles.arrow_wrapper, {opacity: this.state.opacity, left: arrow_left, top: arrow_top}]}>
+          <TouchableWithoutFeedback
+            style={styles.arrow_wrapper}
+            onPress={this.arrowOnPress.bind(this)}>
+            <Animated.Image 
+              source={require('./assets/shooter.png')}
+              style={[{width: 50, height: 50}, {
+                  transform: [{rotate: spin}]
+                }]}
+              resizeMode='contain'
+            />
+          </TouchableWithoutFeedback>
+        </Animated.View>
 
         <Animated.View style={[styles.button_wrapper, {position: 'absolute', left: _left, top: _top}]} >
           <TouchableOpacity
+            disabled={this.state.disabled}
             onPress={this.onPress.bind(this)}
             style={[styles.ratio_button]} />
           <Text style={{marginLeft: 5, color: 'grey'}} >
@@ -53,6 +156,10 @@ export default class ShooterButton extends Component {
   }
 
   onPress() {
+    this.setState({disabled: true});
+
+    this.fadeIn();
+
     Animated.timing(
       this.state.left,
       {
@@ -65,7 +172,7 @@ export default class ShooterButton extends Component {
     Animated.timing(
       this.state.top,
       {
-        toValue: this._random(0, window.height-80),
+        toValue: this._random(window.height/2 +100, window.height-80),
         duration: 100,
         easing: Easing.quad.inOut
       }
@@ -94,5 +201,13 @@ const styles = StyleSheet.create({
   button_wrapper: {
     backgroundColor: 'transparent', 
     flexDirection: 'row'
+  },
+  arrow_wrapper: {
+    position: 'absolute', 
+    width: 71, 
+    height: 71, 
+    borderRadius: 35.5, 
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 });
